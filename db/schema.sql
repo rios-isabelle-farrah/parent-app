77522b6310958842cb184463f9b8d74818673472
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2022-05-04 21:18:13 -0400

    schema and seed populated

---
+++ db/schema.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS parent_files_db;
+
+CREATE DATABASE parent_files_db;
+
+\c parent_files_db;
+
+CREATE TABLE
+    files(
+        id SERIAL PRIMARY key,
+        child_name TEXT not null,
+        uid TEXT not null,
+        additional_info TEXT not null
+    );
+
+    CREATE TABLE
+    contacts(
+        id SERIAL PRIMARY key,
+        contact_name TEXT NOT NULL,
+        phone TEXT NOT NULL,
+        file_id INT REFERENCES files (id) ON DELETE CASCADE,
+        email TEXT NOT NULL,
+        date TEXT 
+    );
+
+
+ CREATE TABLE
+    reimbursements(
+        id SERIAL PRIMARY key,
+        category TEXT NOT NULL,
+        details TEXT NOT NULL,
+        file_id INT REFERENCES files (id) ON DELETE CASCADE,
+        amount_spent INT NOT NULL,
+        date TEXT 
+    );
+
+
+     CREATE TABLE
+    gut_health(
+        id SERIAL PRIMARY key,
+        category TEXT NOT NULL,
+        file_id INT REFERENCES files (id) ON DELETE CASCADE,
+        details TEXT NOT NULL,
+        date TEXT 
+    );
+
+
+CREATE TABLE
+    meetings(
+        id SERIAL PRIMARY key,
+        file_id INT REFERENCES files (id) ON DELETE CASCADE,
+        date TEXT,
+        category TEXT not null,	
+        details TEXT NOT NULL
+    );
